2a5888ccb8eb2f3148daf92b8286610965887e64
nelt <nel.taurisson@gmail.com>
2015-08-20 07:30:28 +0200

    preparing zk impl by extracting some in memory code to engine-commons

--- poomjobs-engine-commons/src/main/java/org/codingmatters/poomjobs/engine/EngineConfiguration.java
+++ poomjobs-engine-commons/src/main/java/org/codingmatters/poomjobs/engine/EngineConfiguration.java
@@ -8,10 +8,18 @@ import java.time.Duration;
  */
 public class EngineConfiguration {
     static public final Long DEFAULT_DEFAULT_RETENTION_DELAY = Duration.ofHours(2).toMinutes();
+    private final Long defaultRetentionDelay;
 
+    public EngineConfiguration(Long defaultRetentionDelay) {
+        this.defaultRetentionDelay = defaultRetentionDelay;
+    }
 
     static public Builder defaults() {
         return new Builder();
+    }
+
+    public Long getDefaultRetentionDelay() {
+        return defaultRetentionDelay;
     }
 
     static public class Builder {
@@ -25,16 +33,6 @@ public class EngineConfiguration {
         public EngineConfiguration config() {
             return new EngineConfiguration(this.defaultRetentionDelay);
         }
-    }
-
-    private final Long defaultRetentionDelay;
-
-    public EngineConfiguration(Long defaultRetentionDelay) {
-        this.defaultRetentionDelay = defaultRetentionDelay;
-    }
-
-    public Long getDefaultRetentionDelay() {
-        return defaultRetentionDelay;
     }
 
 }
