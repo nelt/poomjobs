1d07a15edf297c0df3c2efb0c5ae1df4f5c510cd
nelt <nel.taurisson@gmail.com>
2015-12-15 06:57:23 +0100

    SSE monitoring client implementation, need to add acceptance tests

--- poomjobs-engine-rest/src/main/java/org/codingmatters/poomjobs/engine/rest/JerseyRestEngine.java
+++ poomjobs-engine-rest/src/main/java/org/codingmatters/poomjobs/engine/rest/JerseyRestEngine.java
@@ -4,25 +4,97 @@ import org.codingmatters.poomjobs.apis.exception.ServiceException;
 import org.codingmatters.poomjobs.apis.jobs.JobStatus;
 import org.codingmatters.poomjobs.apis.services.monitoring.JobMonitoringService;
 import org.codingmatters.poomjobs.apis.services.monitoring.StatusChangedMonitor;
+import org.codingmatters.poomjobs.service.rest.AtomicString;
+import org.codingmatters.poomjobs.service.rest.api.JsonJobCodec;
+import org.codingmatters.poomjobs.service.rest.api.RestJobStatusChange;
+import org.glassfish.jersey.media.sse.EventSource;
+import org.glassfish.jersey.media.sse.InboundEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+import static javax.ws.rs.client.Entity.entity;
 
 /**
  * Created by nel on 18/11/15.
  */
 public class JerseyRestEngine implements JobMonitoringService {
 
+    static private final Logger log = LoggerFactory.getLogger(JerseyRestEngine.class);
+
     private final Client httpClient;
     private final String baseUrl;
+
+    private final Map<UUID, StatusChangedMonitor> monitors = new HashMap<>();
+
+    private final EventSource eventSource;
+    private final AtomicString clientUuid = new AtomicString(null);
+    private final JsonJobCodec codec = new JsonJobCodec();
+
+    private final long registrationTimeout = 10000;
 
     public JerseyRestEngine(Client httpClient, String baseUrl) {
         this.httpClient = httpClient;
         this.baseUrl = baseUrl;
+
+        this.eventSource = EventSource.target(this.httpClient.target(this.baseUrl).path("/monitoring")).build();
+        this.eventSource.register(this::registered, "uuid");
+        this.eventSource.register(this::statusChanged, "job-status-changed");
     }
 
     @Override
     public JobStatus monitorStatus(UUID uuid, StatusChangedMonitor monitor) throws ServiceException {
-        return null;
+        this.manageEventSource();
+        Response response = this.httpClient.target(this.baseUrl).path(String.format("/%s/monitor/status", uuid.toString()))
+                .request()
+                .post(entity("{\"clientUuid\": \"" + this.clientUuid + "\"}", "application/json"));
+        if(response.getStatusInfo() != Response.Status.OK) {
+            throw new ServiceException("error registering change monitor for job " + uuid + "( clientUuid=" + this.clientUuid.get() + ")");
+        }
+        String json = response.readEntity(String.class);
+        try {
+            return this.codec.readJobStatus(json);
+        } catch (IOException e) {
+            throw new ServiceException("error reading status from response : " + json, e);
+        }
     }
+
+    private synchronized void manageEventSource() throws ServiceException {
+        if(! this.eventSource.isOpen()) {
+            this.eventSource.open();
+        }
+        try {
+            this.clientUuid.waitUntilNot(null, this.registrationTimeout);
+        } catch (InterruptedException | TimeoutException e) {
+            throw new ServiceException("failed registering to event source", e);
+        }
+    }
+
+
+
+    private void registered(InboundEvent event) {
+        this.clientUuid.set(event.readData());
+    }
+
+    private void statusChanged(InboundEvent event) {
+        RestJobStatusChange change;
+        try {
+            change = this.codec.readStatusChange(event.readData());
+        } catch (IOException e) {
+            log.error("error reading status change from event: " + event, e);
+            return;
+        }
+        StatusChangedMonitor monitor = this.monitors.get(change.getJob().getUuid());
+        if(monitor != null) {
+            monitor.statusChanged(change.getJob(), change.getOldStatus());
+        }
+    }
+
 }
--- poomjobs-engine-rest/src/main/java/org/codingmatters/poomjobs/service/rest/AtomicObjectHolder.java
+++ poomjobs-engine-rest/src/main/java/org/codingmatters/poomjobs/service/rest/AtomicObjectHolder.java
@@ -1,5 +1,7 @@
 package org.codingmatters.poomjobs.service.rest;
 
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by nel on 15/12/15.
  */
@@ -14,13 +16,51 @@ public class AtomicObjectHolder<T> {
         return this.holded;
     }
 
-    public synchronized void set(T str) {
-        this.holded = str;
+    public synchronized void set(T holded) {
+        this.holded = holded;
+        this.notifyAll();
     }
 
     public synchronized T getAndSet(T holded) {
         T result = this.holded;
-        this.holded = holded;
+        this.set(holded);
         return result;
+    }
+
+    public synchronized T setIfNull(T holded) {
+        if(this.holded == null) {
+            this.set(holded);
+        }
+        return this.holded;
+    }
+
+    private synchronized T waitWhile(Cond condition, long timeout) throws InterruptedException, TimeoutException {
+        long start = System.currentTimeMillis();
+        while(condition.is() && System.currentTimeMillis() - start < timeout) {
+            this.wait(timeout - (System.currentTimeMillis() - start));
+        }
+        if(condition.is()) {
+            throw new TimeoutException();
+        }
+        return this.holded;
+    }
+    public synchronized T waitUntilNot(T value, long timeout) throws InterruptedException, TimeoutException {
+        return this.waitWhile(() -> this.same(value), timeout);
+    }
+
+    public synchronized T waitWhile(T value, long timeout) throws InterruptedException, TimeoutException {
+        return this.waitWhile(() -> ! this.same(value), timeout);
+    }
+
+    public interface Cond {
+        boolean is();
+    }
+
+    private boolean same(T value) {
+        if(this.holded == null) {
+            return value == null;
+        } else {
+            return this.holded.equals(value);
+        }
     }
 }
