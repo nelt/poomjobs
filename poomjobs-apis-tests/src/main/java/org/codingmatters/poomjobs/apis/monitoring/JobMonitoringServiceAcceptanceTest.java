e271b4728ace49c921b5ed8f730619c46afd443f
nelt <nel.taurisson@gmail.com>
2015-12-17 05:50:54 +0100

    added acceptance tests for multiple monitors

--- poomjobs-apis-tests/src/main/java/org/codingmatters/poomjobs/apis/monitoring/JobMonitoringServiceAcceptanceTest.java
+++ poomjobs-apis-tests/src/main/java/org/codingmatters/poomjobs/apis/monitoring/JobMonitoringServiceAcceptanceTest.java
@@ -3,18 +3,18 @@ package org.codingmatters.poomjobs.apis.monitoring;
 import org.codingmatters.poomjobs.apis.PoorMansJob;
 import org.codingmatters.poomjobs.apis.TestConfigurationProvider;
 import org.codingmatters.poomjobs.apis.exception.NoSuchJobException;
+import org.codingmatters.poomjobs.apis.jobs.Job;
 import org.codingmatters.poomjobs.apis.jobs.JobStatus;
 import org.codingmatters.poomjobs.apis.services.monitoring.JobMonitoringService;
 import org.codingmatters.poomjobs.apis.services.queue.JobQueueService;
-import org.codingmatters.poomjobs.apis.services.queue.JobSubmission;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 import static org.codingmatters.poomjobs.apis.jobs.JobStatus.PENDING;
 import static org.codingmatters.poomjobs.apis.jobs.JobStatus.RUNNING;
+import static org.codingmatters.poomjobs.apis.services.queue.JobSubmission.job;
 import static org.codingmatters.poomjobs.test.utils.TestHelpers.assertBefore;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
@@ -38,7 +38,7 @@ public abstract class JobMonitoringServiceAcceptanceTest {
         this.queue = PoorMansJob.queue(config.getQueueConfig());
         this.monitoring = PoorMansJob.monitor(config.getMonitorConfig());
 
-        this.jobUuid = this.queue.submit(JobSubmission.job("job").submission()).getUuid();
+        this.jobUuid = this.queue.submit(job("job").submission()).getUuid();
     }
 
     @Test
@@ -53,7 +53,7 @@ public abstract class JobMonitoringServiceAcceptanceTest {
 
         @Test
     public void testMonitor() throws Exception {
-        final HashMap<String, JobStatus> change = new HashMap<>();
+        final Map<String, JobStatus> change = Collections.synchronizedMap(new HashMap<>());
 
         this.monitoring.monitorStatus(this.jobUuid, ((job, old) -> {
             change.put("from", old);
@@ -67,6 +67,38 @@ public abstract class JobMonitoringServiceAcceptanceTest {
 
         assertBefore(() -> change.get("from"), is(PENDING), 1000);
         assertBefore(() -> change.get("to"), is(RUNNING), 1000);
+    }
+
+    @Test
+    public void testTwoMonitorsOnDifferentJob() throws Exception {
+        Job job2 = this.queue.submit(job("job").submission());
+
+        final List<String> received = Collections.synchronizedList(new LinkedList<>());
+
+        this.monitoring.monitorStatus(this.jobUuid, (job, old) -> received.add("one"));
+        this.monitoring.monitorStatus(job2.getUuid(), (job, old) -> received.add("two"));
+
+        this.queue.start(this.jobUuid);
+
+        assertBefore(() -> received.size(), is(1), 1000);
+        assertBefore(() -> received.get(0), is("one"), 1000);
+
+        this.queue.start(job2.getUuid());
+
+        assertBefore(() -> received.size(), is(2), 1000);
+        assertBefore(() -> received.get(1), is("two"), 1000);
+    }
+
+    @Test
+    public void testTwoMonitorsOnSameJob() throws Exception {
+        final List<String> received = Collections.synchronizedList(new LinkedList<>());
+
+        this.monitoring.monitorStatus(this.jobUuid, (job, old) -> received.add("one"));
+        this.monitoring.monitorStatus(this.jobUuid, (job, old) -> received.add("two"));
+
+        this.queue.start(this.jobUuid);
+
+        assertBefore(() -> received.size(), is(2), 1000);
     }
 
 
